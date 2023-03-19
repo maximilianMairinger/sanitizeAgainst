63dd8309d3e6b3f331561c204a6737317bc67ae9
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-03-19 21:28:45 +0100

    Build and CI update

--- rollup.node.test.config.js
+++ rollup.node.test.config.js
@@ -1,11 +1,25 @@
-import { merge } from "webpack-merge"
-import commonMod from "./rollup.node.common.config"
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonJS from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 
 
-export default merge(commonMod, {
+export default {
   input: 'test/src/test.ts',
   output: {
     file: 'test/dist/test.js',
     format: 'cjs',
+    sourcemap: true
   },
-})
+  plugins: [
+    typescript({tsconfig: "./tsconfig.test.json", noEmitOnError: false, sourceMap: true}), 
+    resolve({modulesOnly: true, preferBuiltins: true}),
+    commonJS({
+      include: 'node_modules/**'
+    }),
+    json()
+  ],
+  // dont log errors
+  onwarn: () => {}
+};
+
